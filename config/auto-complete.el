ceced9a996acf6110f2f4dc48977a16bbcd13fe1
pedrofrohmut <pedrofrohmut@gmail.com>
2026-04-26 11:56:46 -0300

    Added copilot to auto complete

--- config/auto-complete.el
+++ config/auto-complete.el
@@ -17,30 +17,8 @@
 
 (use-package corfu
   :ensure t
-  ;; Optional customizations
-  ;; :custom
-  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
-  ;; (corfu-auto t)                 ;; Enable auto completion
-  ;; (corfu-separator ?\s)          ;; Orderless field separator
-  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
-  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
-  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
-  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
-  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
-
-  ;; Enable Corfu only for certain modes. See also `global-corfu-modes'.
-  ;; :hook ((prog-mode . corfu-mode)
-  ;;        (shell-mode . corfu-mode)
-  ;;        (eshell-mode . corfu-mode))
-
-  ;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
-  ;; be used globally (M-/).  See also the customization variable
-  ;; `global-corfu-modes' to exclude certain modes.
   :init
   (global-corfu-mode))
-
-;(keymap-global-set "C-j" 'completion-at-point)
 
 ;; Cape #########################################################################
 
@@ -50,6 +28,19 @@
   :init
   (add-hook 'completion-at-point-functions #'cape-dabbrev)
   (add-hook 'completion-at-point-functions #'cape-file)
-  (add-hook 'completion-at-point-functions #'cape-elisp-block)
-  ;; (add-hook 'completion-at-point-functions #'cape-history)
-  )
+  (add-hook 'completion-at-point-functions #'cape-elisp-block))
+
+;; Copilot ######################################################################
+
+(use-package copilot
+  :ensure t
+  :bind (:map copilot-completion-map
+              ("C-j" . copilot-accept-completion)
+              ("C-l" . copilot-accept-completion-by-line)
+              ("C-w" . copilot-accept-completion-by-word)
+              ("C-k" . copilot-clear-overlay)
+              ("C-n" . copilot-next-completion)
+              ("C-p" . copilot-previous-completion)))
+
+(keymap-global-set "C-; i" 'copilot-complete)
+(keymap-global-set "C-; o" 'copilot-clear-overlay)
