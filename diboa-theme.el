63ea9c3c761d525b835187ce1dd420f2184c6088
pedrofrohmut <pedrofrohmut@gmail.com>
2026-06-30 13:58:54 -0300

    Completed my custom theme with autothemer; Tested with elisp and c++

--- diboa-theme.el
+++ diboa-theme.el
@@ -1,18 +1,19 @@
 (require 'autothemer)
 (require 'highlight-numbers)
 
-(autothemer-deftheme
- diboa
- "A theme that is quiet and calm"
+(autothemer-deftheme diboa
+ "A dark theme that is quiet and calm"
 
  ((((class color) (min-colors #xFFFFFF))) ;; Only supports GUI for now
   ;; Define color palette
   (diboa-bg              "#1a1b26")
+  (diboa-lighter-bg      "#3a3b46")
   (diboa-fg              "#cecece")
   (diboa-yellow          "#e5c463")
   (diboa-red             "#f85e84")
   (diboa-bright-red      "#ff0000")
-  (diboa-dark-gray       "#181818")
+  (diboa-darker-gray     "#181818")
+  (diboa-dark-gray       "#242424")
   (diboa-medium-gray     "#666666")
   (diboa-lighter-gray    "#fefefe")
   (diboa-gray-blue       "#557799")
@@ -20,6 +21,7 @@
   (diboa-light-blue      "#4178b5")
   (diboa-light-green     "#99ce99")
   (diboa-light-green2    "#43a047")
+  (diboa-light-purple    "#ce9fce")
   )
 
  ;; Customize faces
@@ -28,29 +30,46 @@
   (fringe (:background diboa-bg))
 
   (font-lock-variable-name-face (:foreground diboa-fg))
+  (font-lock-type-face          (:foreground diboa-fg))
 
-  (cursor  (:foreground diboa-dark-gray :background diboa-yellow))
+  (cursor  (:foreground diboa-darker-gray :background diboa-yellow))
 
-  (font-lock-builtin-face (:foreground diboa-lighter-blue))
-  (font-lock-keyword-face (:foreground diboa-lighter-blue))
+  ;; keywords
+  (font-lock-builtin-face       (:foreground diboa-lighter-blue))
+  (font-lock-keyword-face       (:foreground diboa-lighter-blue))
+  (font-lock-function-name-face (:foreground diboa-lighter-blue))
 
+  ;; Comments
   (font-lock-comment-face    (:foreground diboa-gray-blue))
-  (font-lock-doc-facel       (:foreground diboa-gray-blue))
+  (font-lock-doc-face        (:foreground diboa-gray-blue))
   (font-lock-doc-markup-face (:foreground diboa-gray-blue))
 
+  ;; strings
   (font-lock-string-face    (:foreground diboa-light-green))
-  (font-lock-constant-face  (:foreground diboa-light-green))
-  (highlight-numbers-number (:foreground diboa-light-green))
-  ;; (set-face-foreground 'highlight-numbers-number clr-light-purple)
-  ;; (set-face-foreground 'highlight-numbers-number "#ce99ce")
 
+  ;; numbers and values
+  (font-lock-constant-face  (:foreground diboa-fg))
+  (highlight-numbers-number (:foreground diboa-fg))
+
+  (font-lock-constant-face  (:foreground diboa-yellow))
+  (highlight-numbers-number (:foreground diboa-yellow))
+
+  ;; Line number
   (line-number              (:background diboa-bg :foreground diboa-medium-gray))
   (line-number-current-line (:background diboa-bg :foreground diboa-yellow))
 
+  ;; Selection
   (region         (:background diboa-light-blue   :foreground diboa-lighter-gray))
   (isearch        (:background diboa-light-green2 :foreground diboa-lighter-gray))
   (lazy-highlight (:background diboa-light-green2 :foreground diboa-lighter-gray))
   (match          (:background diboa-light-green2 :foreground diboa-lighter-gray))
+
+  ;; Mode line
+  (mode-line-active   (:background diboa-lighter-bg :foreground diboa-fg))
+  (mode-line-inactive (:background diboa-bg         :foreground diboa-medium-gray))
+
+  ;; Prompt
+  (minibuffer-prompt (:foreground diboa-lighter-blue))
   )
  )
 
