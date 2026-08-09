01556ffd3dd2758234b818e16d4b2b7bc8b3ca91
pedrofrohmut <pedrofrohmut@gmail.com>
2026-08-09 13:33:43 -0300

    Backup OLD

---
+++ OLD/diboa-theme.el
@@ -0,0 +1,84 @@
+(require 'autothemer)
+(require 'highlight-numbers)
+
+(autothemer-deftheme diboa
+ "A dark theme that is quiet and calm"
+
+ ((((class color) (min-colors #xFFFFFF))) ;; Only supports GUI for now
+  ;; Define color palette
+  (diboa-bg              "#1a1b26")
+  (diboa-lighter-bg      "#3a3b46")
+  (diboa-fg              "#cecece")
+  (diboa-yellow          "#e5c463")
+  (diboa-red             "#f85e84")
+  (diboa-bright-red      "#ff0000")
+  (diboa-darker-gray     "#181818")
+  (diboa-dark-gray       "#242424")
+  (diboa-medium-gray     "#666666")
+  (diboa-lighter-gray    "#fefefe")
+  (diboa-gray-blue       "#557799")
+  (diboa-lighter-blue    "#aaccee")
+  (diboa-light-blue      "#4178b5")
+  (diboa-light-green     "#99ce99")
+  (diboa-light-green2    "#43a047")
+  (diboa-light-purple    "#ce9fce")
+  )
+
+ ;; Customize faces
+ ((default (:foreground diboa-fg :background diboa-bg))
+
+  (fringe (:background diboa-bg))
+
+  ;; Removing colors where not desired
+  (font-lock-variable-name-face (:foreground diboa-fg))
+  (font-lock-function-name-face (:foreground diboa-fg))
+  (font-lock-type-face          (:foreground diboa-fg))
+  (eglot-semantic-static        (:foreground diboa-fg))
+  (eglot-semantic-namespace     (:foreground diboa-fg))
+
+  (cursor  (:foreground diboa-darker-gray :background diboa-yellow))
+
+  ;; keywords
+  (font-lock-builtin-face       (:foreground diboa-lighter-blue))
+  (font-lock-keyword-face       (:foreground diboa-lighter-blue))
+  (font-lock-function-name-face (:foreground diboa-lighter-blue))
+
+  ;; Comments
+  (font-lock-comment-face    (:foreground diboa-gray-blue))
+  (font-lock-doc-face        (:foreground diboa-gray-blue))
+  (font-lock-doc-markup-face (:foreground diboa-gray-blue))
+
+  ;; strings
+  (font-lock-string-face    (:foreground diboa-light-green))
+
+  ;; numbers and values
+  (font-lock-constant-face  (:foreground diboa-fg))
+  (highlight-numbers-number (:foreground diboa-fg))
+
+  (font-lock-constant-face  (:foreground diboa-yellow))
+  (highlight-numbers-number (:foreground diboa-yellow))
+
+  ;; Line number
+  (line-number              (:background diboa-bg :foreground diboa-medium-gray))
+  (line-number-current-line (:background diboa-bg :foreground diboa-yellow))
+
+  ;; Selection
+  (region         (:background diboa-light-blue   :foreground diboa-lighter-gray))
+  (isearch        (:background diboa-light-green2 :foreground diboa-lighter-gray))
+  (lazy-highlight (:background diboa-light-green2 :foreground diboa-lighter-gray))
+  (match          (:background diboa-light-green2 :foreground diboa-lighter-gray))
+
+  ;; Mode line
+  (mode-line-active   (:background diboa-lighter-bg :foreground diboa-fg))
+  (mode-line-inactive (:background diboa-bg         :foreground diboa-medium-gray))
+
+  ;; Prompt
+  (minibuffer-prompt (:foreground diboa-lighter-blue))
+
+  ;; Tuareg
+  (tuareg-font-lock-governing-face (:foreground diboa-lighter-blue))
+  (tuareg-font-lock-operator-face (:foreground diboa-lighter-blue))
+  )
+ )
+
+(provide-theme 'diboa)
---
+++ OLD/init.el
@@ -0,0 +1,219 @@
+;; Setup Garbage collection back to normal after init
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (* 800 000))))
+
+;; Setup package ################################################################
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org"   . "https://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("elpa"  . "https://elpa.gnu.org/packages/"))
+(package-initialize)
+
+;; Emacs options ################################################################
+
+(setq inhibit-startup-message t)
+
+;; Line numbers
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode t)
+
+;; Identation
+(setq-default indent-tabs-mode nil
+              tab-always-indent t
+              c-tab-always-indent t
+              tab-width 4)
+
+;; Delete the selected text when start typing
+(delete-selection-mode 1)
+
+;; Maching highlight
+(show-paren-mode t)
+(setq show-paren-delay 0)
+(setq show-paren-style 'parenthesis)
+(set-face-attribute 'show-paren-match nil :foreground "#f00" :background "#1a1b2c" :weight 'ultra-bold)
+
+;; Update emacs buffers when the files change outside (keep buffers sync)
+(setq global-auto-revert-non-file-buffers t)
+(global-auto-revert-mode t)
+
+;; Saves your location in files for the next time you open it
+(save-place-mode t)
+
+;; Show cursor position in statusbar
+(setq column-number-mode t)
+
+;; On file save
+(add-hook 'write-file-hooks 'delete-trailing-whitespace)
+
+;; Files
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+;; Set root dir for project.el
+(setq project-vc-extra-root-markers '(".project.el" ".projectile" ))
+
+;; Close emacs on the extended commands
+(defun quit-emacs ()
+  (interactive)
+  (save-buffers-kill-emacs))
+
+;; Appearance ###################################################################
+
+;; Fonts
+(set-face-attribute 'default nil :font "FiraMono Nerd Font" :height 104)
+;; (set-face-attribute 'default nil :font "Inconsolata Nerd Font" :height 120)
+
+;; Cursor
+(blink-cursor-mode 0)
+
+;; Highlight numbers (emacs do not have a face for numbers by default)
+(use-package highlight-numbers
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook 'highlight-numbers-mode))
+;; (require 'highlight-numbers)
+;; (set-face-foreground 'highlight-numbers-number clr-light-purple)
+;; (set-face-foreground 'highlight-numbers-number "#ce99ce")
+
+;; Fix bugged colors in compile mode
+(add-hook 'compilation-filter-hook 'ansi-color-compilation-filter)
+
+;; Fixing Git fringe colors
+(require 'diff-hl)
+(set-face-background 'diff-hl-insert "#262")
+(set-face-background 'diff-hl-delete "#622")
+(set-face-background 'diff-hl-change "#266")
+
+;; Whitespaces ##################################################################
+
+(use-package whitespace
+  :ensure t
+  :config
+  (setq-default whitespace-style '(face tabs tab-mark trailing))
+  (set-face-background 'whitespace-trailing "#333")
+  (set-face-background 'whitespace-tab "#357")
+  ;; (global-whitespace-mode t)
+  )
+
+;; Syntax Subword ###############################################################
+
+;; Changes the word concept in emacs that affects M-b and M-f and other word
+;; functions and keymaps
+;; (use-package syntax-subword
+;;   :ensure nil
+;;   :defer t
+;;   :init
+;;   (setq syntax-subword-skip-spaces t)
+;;   ;; :hook (prog-mode . syntax-subword-mode)
+;;   )
+
+;; Undo Tree ####################################################################
+
+;; (use-package undo-tree
+;;   :ensure t
+;;   :config
+;;   (global-undo-tree-mode)
+;;   (setq undo-tree-visualizer-timestamps t
+;;         undo-tree-auto-save-history nil
+;;         undo-tree-visualizer-diff t))
+
+;; Surround #####################################################################
+
+(use-package surround
+  :ensure t
+  :bind-keymap ("C-," . surround-keymap))
+
+;; Editorconfig #################################################################
+
+;; (use-package editorconfig
+;;   :ensure t
+;;   :config
+;;   (editorconfig-mode 1))
+
+;; Move Text ####################################################################
+
+;; (use-package move-text
+;;   :ensure t
+;;   :bind
+;;   (("M--" . move-text-region-up)
+;;    ("M-=" . move-text-region-down)))
+
+;; Multicursors #################################################################
+
+;; (use-package multiple-cursors
+;;   :ensure t
+;;   :bind
+;;   (("C-<" .   'mc/skip-to-next-like-this)
+;;    ("C->" .   'mc/mark-next-like-this)
+;;    ("C-c e" . 'mc/edit-lines)
+;;    :map mc/keymap
+;;    ("RET" . nil)
+;;    ("<return>" . nil))
+;;   :config
+;;   (add-to-list 'mc/cmds-to-run-for-all 'self-insert-command))
+
+;; Fill Column ##################################################################
+
+(setq-default fill-column 141)
+(global-display-fill-column-indicator-mode t)
+(setq-default display-fill-column-indicator-column 81)
+
+(use-package visual-fill-column
+  :ensure t)
+
+(setq-default visual-fill-column-center-text t)
+
+(add-hook 'after-init-hook 'global-visual-fill-column-mode)
+
+;; Highlight Numbers ############################################################
+
+;; Emacs does not have this group by default
+(use-package highlight-numbers
+  :ensure t)
+
+;; Loading  #####################################################################
+
+;; TODO: Check load function if is the right to use
+;; TODO: Make a terminal file to easy open a terminal in a horizontal split
+
+(load "~/.config/emacs/config/my-custom.el")
+
+(load "~/.config/emacs/config/keybinds.el")
+
+;; (load "~/.config/emacs/config/colors.el")
+;; (load "~/.config/emacs/config/alt-colors.el")
+(load "~/.config/emacs/config/custom-theme.el")
+
+(load "~/.config/emacs/config/lsp.el")
+
+(load "~/.config/emacs/config/debug.el")
+
+(load "~/.config/emacs/config/symbols-outline.el")
+
+(load "~/.config/emacs/config/auto-complete.el")
+
+(load "~/.config/emacs/config/languages.el")
+
+(load "~/.config/emacs/config/snippets.el")
+
+(load "~/.config/emacs/config/mini-buffer.el")
+
+(load "~/.config/emacs/config/prescient.el")
+
+(load "~/.config/emacs/config/github.el")
+
+(load "~/.config/emacs/config/consult.el")
+
+(load "~/.config/emacs/config/avy.el")
+
+(load "~/.config/emacs/config/dired.el")
+
+(load "~/.config/emacs/config/integrate-terminals.el")
+
+(load "~/.config/emacs/config/evil.el")
+
+;; Set customize file
+(setq custom-file "~/.config/emacs/emacs-custom.el")
+(load custom-file :noerror)
